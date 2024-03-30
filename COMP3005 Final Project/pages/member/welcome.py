153be467cd4d838ffdb69f38ba6266a27d036f5c
Lujain23 <lujainsharafeldin23@gmail.com>
2024-03-30 01:39:57 -0400

    Update welcome.py
    FRONTEND added the member buttons in a good way
    (they have no functionality yet though)

--- COMP3005 Final Project/pages/member/welcome.py
+++ COMP3005 Final Project/pages/member/welcome.py
@@ -11,10 +11,25 @@ def joinClass():
 def rescheduleClass():
 def cancelClass():
 '''
-
+rowStyle = {'fontSize':'20px','textAlign': 'center', 'width':'200px','height':'100px'}
 layout = html.Div([
-    html.H1('Welcome to Health and Fitness Club Management System!'),
+    html.H1('Welcome to the Health and Fitness Club!'),
     html.Div([
-        html.P('Hello')
-    ])
+        html.Table([
+            html.Tr([
+                html.Td(html.Button('Update Personal Information', id='updateInfoButton', n_clicks=0, style=rowStyle)),
+                html.Td(html.Button('Join Class',id='joinClassButton',n_clicks=0, style=rowStyle))
+            ]),
+            html.Tr([
+                html.Td(html.Button('Print Fitness and Routine',id='printExerciseButton',n_clicks=0, style=rowStyle)),
+                html.Td(html.Button('Reschedule Class',id='rescheduleClassButton',n_clicks=0, style=rowStyle)),
+
+            ]),  
+            html.Tr([
+                html.Td(html.Button('Print Health Statistics',id='printHealthButton',n_clicks=0, style=rowStyle)),
+                html.Td(html.Button('Cancel Class',id='cancelClassButton',n_clicks=0, style=rowStyle))
+            ]),             
+        ], style = {'paddingLeft':'100px'}
+        ) #end of table
+    ])#end of div
 ])
